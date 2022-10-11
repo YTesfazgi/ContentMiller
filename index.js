0d8bf2056b0ad05a4ef4dceff4281d59414cf2ff
Yohana Tesfazgi <yytesfazgi@gmail.com>
2022-10-11 15:17:10 -0400

    now calls Video and Comment Thread APIs

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import 'dotenv/config'
 import path from 'path'
 import Fastify from 'fastify'
 import fetch from "node-fetch"
-import {google} from 'googleapis'
 import fastifyFormbody from '@fastify/formbody'
 import fastifyStatic from '@fastify/static'
 import fastifyPostgres from '@fastify/postgres'
@@ -37,17 +36,56 @@ fastify.get('/', function (request, reply) {
 fastify.get('/search', function (request, reply) {   
     var searchTerm = String(request.query.search_term);
 
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchTerm}&key=${process.env.API_KEY}`, {
+    // calls Search API, finds video IDs for the other API calls
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${searchTerm}&type=video&key=${process.env.API_KEY}`, {
         method: 'GET',
         headers: {
             'Accept':'application/json'
         }
     })
     .then((response) => response.json())
-    .then((json) => console.log(json.items))
+    .then((json) => {
+        // creates array of videoIds
+        var jsonLength = Object.keys(json.items).length
+        var videoIds = [];
 
-    reply.header('Content-Type', 'text/html')
-    reply.view("./views/homepage.hbs")
+        for (var i = 0; i < jsonLength; i++) {
+            videoIds.push(json.items[i].id.videoId);
+        }
+        
+        // creates string for concatenating to Video API call
+        var string = videoIds.join("%2C");
+        
+        // calls Video API, responds with all video info
+        fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id=${string}&key=${process.env.API_KEY}`, {
+            method: 'GET',
+            headers: {
+                'Accept':'application/json'
+            }
+        })
+        .then((response) => response.json())
+        .then((vidInfo) => console.log(vidInfo.items))
+
+        // calls Comment Thread API, responds with top comment threads (can only do 1 video ID per request, must loop)
+        for (var i = 0; i < jsonLength; i++) {
+            fetch(`https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=3&videoId=${videoIds[i]}&key=${process.env.API_KEY}`, {
+                method: 'GET',
+                headers: {
+                    'Accept':'application/json'
+                }
+            })
+            .then((response) => response.json())
+            .then((json) => {
+                try {
+                    console.log(json.items)
+                } catch (err){
+                    console.error(err)
+                }
+            })
+        }
+        reply.header('Content-Type', 'text/html')
+        reply.view("./views/searchresults.hbs", { items: json.items, searchTerm: searchTerm })    
+    })
 })
 
 // Run the server
